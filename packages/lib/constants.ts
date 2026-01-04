a5fec20580d359bd4a606d8b4c4b57e75366631d
Xavier Mas Leszkiewicz <xavier.mas.le@gmail.com>
2026-01-05 02:24:00 +0400

    Fix

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -125,7 +125,7 @@ export const API_NAME_LENGTH_MAX_LIMIT = 80;
 export const MINUTES_TO_BOOK = process.env.NEXT_PUBLIC_MINUTES_TO_BOOK || "5";
 export const ENABLE_PROFILE_SWITCHER = process.env.NEXT_PUBLIC_ENABLE_PROFILE_SWITCHER === "1";
 // Needed for orgs
-export const ALLOWED_HOSTNAMES = JSON.parse(`[${process.env.ALLOWED_HOSTNAMES || ""}]`) as string[];
+export const ALLOWED_HOSTNAMES = process.env.ALLOWED_HOSTNAMES ? process.env.ALLOWED_HOSTNAMES.trim().split(',') : [];
 export const RESERVED_SUBDOMAINS = JSON.parse(`[${process.env.RESERVED_SUBDOMAINS || ""}]`) as string[];
 
 export const ORGANIZATION_SELF_SERVE_PRICE = parseFloat(
